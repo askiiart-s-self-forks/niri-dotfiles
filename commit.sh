11acbd0a533cbed92e99628f6315ec6e9c4a2cb2
askiiart <dev@askiiart.net>
2023-10-09 09:42:05 -0500

    Add commit script

---
+++ commit.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+GIT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+cd $GIT_DIR
+git add .
+git commit -m "Automated backup: $(date +'%Y-%m-%d %H:%M:%S')"
+git push origin main
