d641450fd63ab8518df49f07014e14619c3a2164
askiiart <dev@askiiart.net>
2023-10-09 10:05:01 -0500

    Automated backup: 2023-10-09 10:05:01

--- create-cron-job.sh
+++ create-cron-job.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 GIT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-crontab -l | grep -q "* * * * * $GIT_DIR/commit.sh" || (crontab -l 2>/dev/null; echo "* * * * * $GIT_DIR/commit.sh") | crontab -
+(crontab -l 2>/dev/null; echo "* * * * * $GIT_DIR/commit.sh") | crontab -
