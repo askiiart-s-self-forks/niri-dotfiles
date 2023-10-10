5e49d472464f0d4a1e241969727e229c5308b797
askiiart <dev@askiiart.net>
2023-10-10 12:53:31 -0500

    Automated backup: 2023-10-10 17:53:31+00:00

--- commit.sh
+++ commit.sh
@@ -2,6 +2,7 @@
 set -e
 GIT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cd $GIT_DIR
+./backup.sh
 git add .
 git commit -m "Automated backup: $(date --rfc-3339=s --utc)"
 git push origin main
--- fish/conf.d/custom.fish
+++ fish/conf.d/custom.fish
@@ -1,1 +1,3 @@
 fish_add_path .cargo/bin/
+set -x GPG_TTY (tty)
+set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
