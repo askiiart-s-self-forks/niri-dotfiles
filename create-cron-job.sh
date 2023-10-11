572a10c961e4055a454cc078d00ffb5d75a966fb
askiiart <dev@askiiart.net>
2023-10-10 20:25:23 -0500

    Update scripts

--- create-cron-job.sh
+++ create-cron-job.sh
@@ -2,3 +2,7 @@
 set -e
 GIT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 (crontab -l 2>/dev/null; echo "* * * * * $GIT_DIR/commit.sh") | crontab -
+
+############################################################################
+# NOTE: commit signing must be turned off, or not require any interaction. #
+############################################################################
