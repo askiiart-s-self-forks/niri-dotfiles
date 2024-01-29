a269c6cba72cdc9249d6443d3a3bba341a069d0b
askiiart <dev@askiiart.net>
2024-01-29 15:04:38 -0600

    Add polkit stuff for arch-sway

--- arch-sway.sh
+++ arch-sway.sh
@@ -6,7 +6,7 @@ fi
 GIT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 # gnome-keyring is for VS Code
-yay -S gnome-keyring --noconfirm --needed
+yay -S gnome-keyring lxqt-policykit --noconfirm --needed
 yay -S i3status j4-dmenu-desktop swaylock swaybg swaync clipman --noconfirm --needed
 yay -S pipewire-pulse pavucontrol blueman bluetooth-support qpwgraph --noconfirm --needed
 yay -S brightnessctl --noconfirm --needed
@@ -21,6 +21,7 @@ git clone --depth=1 https://github.com/catppuccin/grub
 sudo cp -r ./grub/src/* /usr/share/grub/themes/
 rm -rf grub/
 sudo sed -i 's/#GRUB_THEME="\/path\/to\/gfxtheme"/GRUB_THEME=\/usr\/share\/grub\/themes\/catppuccin-mocha-grub-theme\/theme.txt/g' /etc/default/grub
+sudo sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # greetd, replaced by ly which should be preinstalled
