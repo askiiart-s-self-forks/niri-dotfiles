f47510f9ca0c098777795ea5d2eb8d40b109c53b
askiiart <dev@askiiart.net>
2024-09-23 14:53:13 -0500

    Fix multi-monitor sway configs

--- sway/lid-close.sh
+++ sway/lid-close.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 swaylock -f -i ~/.config/sway/background.png
 systemctl suspend
-iwctl station wlan0 connect Wifey
---
+++ sway/switch-workspace.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+swaymsg "workspace number ${1}; move workspace to output 'Acer Technologies EK271U E 14160766B3W01'"
