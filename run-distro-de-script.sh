a3aacb7415f889e6f32d7b12fac665c793a5c42f
askiiart <dev@askiiart.net>
2024-09-27 09:39:04 -0500

    minor inconsequential updates

--- run-distro-de-script.sh
+++ run-distro-de-script.sh
@@ -4,12 +4,15 @@ command_exists() { type "$1" &>/dev/null; }
 
 cd distro-specific-scripts/
 distro=""
+
 if command_exists "pacman"; then
     distro="arch"
 elif command_exists "dnf" || command_exists "rpm-ostree"; then
     distro="fedora"
 elif command_exists "xbps-install"; then
     distro="void"
+else
+    exit
 fi
 
 ./${distro}-${XDG_CURRENT_DESKTOP}.sh
