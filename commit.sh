6905f49605ba6ed95cef6a5b653b3759be589eb1
askiiart <dev@askiiart.net>
2024-11-06 10:46:18 -0600

    don't need this at all. it's bad

--- commit.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-GIT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-cd $GIT_DIR
-./backup.sh
-git add .
-git commit -m "Automated backup: $(date --rfc-3339=s --utc)"
-git push
-cd -
