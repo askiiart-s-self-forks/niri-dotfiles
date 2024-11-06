25aa0969b4d110b606aa5f91945ed34531b0bdf4
askiiart <dev@askiiart.net>
2024-11-06 10:46:04 -0600

    don't delete everything on close in librewolf

--- librewolf/prefs.js
+++ librewolf/prefs.js
@@ -2,3 +2,4 @@ user_pref("dom.private-attribution.submission.enabled", false);
 user_pref("media.gmp-gmpopenh264.enabled", true);
 user_pref("widget.non-native-theme.scrollbar.size.override", 14);
 user_pref("widget.gtk.overlay-scrollbars.enabled", false);
+user_pref("privacy.sanitize.sanitizeOnShutdown", false);
