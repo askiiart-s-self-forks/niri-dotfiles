aac0a67a54c103e2922b1b8d7b50c24cc8ee62a8
askiiart <dev@askiiart.net>
2024-11-09 22:07:06 -0600

    dark mode librewolf finally (only for librewolf-allow-dark)

--- librewolf/prefs.js
+++ librewolf/prefs.js
@@ -3,3 +3,4 @@ user_pref("media.gmp-gmpopenh264.enabled", true);
 user_pref("widget.non-native-theme.scrollbar.size.override", 14);
 user_pref("widget.gtk.overlay-scrollbars.enabled", false);
 user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+user_pref("privacy.override_rfp_for_color_scheme", true)
